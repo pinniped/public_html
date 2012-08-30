696129e3b251b5831892ad4bfbfe342a60e3bc71
pinniped <abstract.wolf@gmail.com>
2012-08-30 15:58:05 -0400

    Added a template page

---
+++ template.php
@@ -0,0 +1,10 @@
+<?php include('header.php') ?>
+
+<?php // anything included here is put into the head ?>
+<title></title>
+
+<?php include('end_header.php') ?>
+<div id='wrap'>
+</div>
+
+<?php include('footer.php') ?>
