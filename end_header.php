1072d938b8096b9fedb0fe9d3051d226bcbf4bcb
pinniped <abstract.wolf@gmail.com>
2012-08-30 16:09:34 -0400

    Removed main banner img. Removed link locations in nav.

--- end_header.php
+++ end_header.php
@@ -1,84 +1,84 @@
 </head>
 <body>
-<div id='wrap'>
-<div id='header'>
-<a class='banner' href='index' title="The Hannah Group">TM IS AWESOME</a>
-</div>
-<ul id='nav'>
-<li>
-<a href='index'>Home</a>
-</li>
-<li>
-<a href="properties-all">Properties</a>
-<ul class='subnav'>
-<li>
-<a href="properties-for-sale">For Sale</a>
-</li>
-<li>
-<a href="properties-for-lease">For Lease</a>
-</li>
-<li>
-<a href="http://thehannahgroup.idxre.com/sold/52575">Past Properties</a>
-</li>
-<li>
-<a href="http://thehannahgroup.idxre.com/homesearch/52575">Search</a>
-</li>			
-<li class='last'>
-<a href="http://thehannahgroup.idxre.com/openhomes/52575">Open Homes</a>
-</li>
-</ul>
-</li>
-<li>
-<a href="map-search">Map Search</a>
-</li>
-<li>
-<a href="http://thehannahgroup.idxre.com/valuation/52575">Tools</a>
-<ul class='subnav'>
+	<div id='wrap'>
+		<div id='header'>
+			<a class='banner' href='#' title="The Hannah Group">TM IS AWESOME</a>
+		</div>
+		<ul id='nav'>
+			<li>
+			<a href='#'>Home</a>
+			</li>
+			<li>
+			<a href='#'>Properties</a>
+			<ul class='subnav'>
+				<li>
+				<a href='#'>For Sale</a>
+				</li>
+				<li>
+				<a href='#'>For Lease</a>
+				</li>
+				<li>
+				<a href='#'>Past Properties</a>
+				</li>
+				<li>
+				<a href='#'>Search</a>
+				</li>			
+				<li class='last'>
+				<a href='#'>Open Homes</a>
+				</li>
+			</ul>
+			</li>
+			<li>
+			<a href='#'>Map Search</a>
+			</li>
+			<li>
+			<a href='#'>Tools</a>
+			<ul class='subnav'>
 
-<li>
-<a href="http://thehannahgroup.idxre.com/valuation/52575">Valuation Request</a>
-</li>					
-
-
-<li>
-<a href="http://thehannahgroup.idxre.com/login/52575">Property Organizer</a>
-</li>					
+				<li>
+				<a href='#'>Valuation Request</a>
+				</li>					
 
 
-<li>
-<a href="http://thehannahgroup.idxre.com/emailalerts/52575">Email Updates</a>
-</li>
-
-<li class='last'>
-<a href="http://thehannahgroup.idxre.com/mortcalc/52575">Mortgage Calculator</a>
-</li>	
+				<li>
+				<a href='#'>Property Organizer</a>
+				</li>					
 
 
-</ul>		
+				<li>
+				<a href='#'>Email Updates</a>
+				</li>
+
+				<li class='last'>
+				<a href='#'>Mortgage Calculator</a>
+				</li>	
+
+
+			</ul>		
 
 
 
-</li>
-<li>
-<a href="team">Team</a>
-<ul class='subnav'>
+			</li>
+			<li>
+			<a href='#'>Team</a>
+			<ul class='subnav'>
 
-<li>
-<a href="team">Meet the Team</a>
-</li>					
+				<li>
+				<a href='#'>Meet the Team</a>
+				</li>					
 
 
-<li class='last'>
-<a href="info">Info</a>
-</li>
+				<li class='last'>
+				<a href='#'>Info</a>
+				</li>
 
-</ul>			
-</li>
-<li >
-<a href="http://thehannahgroup.blogspot.com/" target="blank">Blog</a>
-</li>
-<li >
-<a href="contact">Contact</a>
-</li>
-</ul>
-<div class='clearfix'></div>
+			</ul>			
+			</li>
+			<li >
+			<a href='#' target="blank">Blog</a>
+			</li>
+			<li >
+			<a href='#'>Contact</a>
+			</li>
+		</ul>
+		<div class='clearfix'></div>
